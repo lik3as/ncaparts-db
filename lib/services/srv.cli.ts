f2bf740fdbce3a302bc97bafb3c6b7ef209e1c20
Davi Almeida <lik3games@gmail.com>
2023-05-18 16:03:10 -0300

    filter is more efficient

--- lib/services/srv.cli.ts
+++ lib/services/srv.cli.ts
@@ -8,15 +8,24 @@ export default class ClienteCtrl implements IFab<Cliente>{
     return await Cliente.findAll({attributes: [name]});
   }
 
+
   async filterUniques(body: Object[] | Object): Promise<Object | Object[] | null> {
-    let emails: string[] = [];
-    (await this.getAttr('email')).forEach((cli, i, arr) => {
-      emails.push(cli.email);
-    })
     if (Array.isArray(body)){
-      return body.filter((bdy) => !emails.includes(bdy.email))
+      const filtered = await Promise.all(
+        body.map(async (bdy) => {
+          //Verifica se não há nenhuma ocorrência deste email no banco de dados
+          return (await this.getBody({ method: 'find_by_', on: 'email', args: bdy.email })) == null;
+        })
+      );   
+
+      return body.filter((_, i) => filtered[i]);   
+
     } else {
-      return (emails.includes((body as any).email)) ? null : body;
+      return (await this.getBody({method: 'find_by_', on: 'email', args: (body as any).email})) != null
+      ?
+      null
+      :
+      body;
     }
   }
 
@@ -30,11 +39,11 @@ export default class ClienteCtrl implements IFab<Cliente>{
 
   public async getBodies({method, on, args}: param_bodies) : body<Cliente[]> { 
     return (typeof args == undefined ) ?
-     Cliente.scope(
+     await Cliente.scope(
       {method: `${method}${on}`}
       ).findAll()
       :
-      Cliente.scope(
+      await Cliente.scope(
       {method: [`${method}${on}`, args]}
       ).findAll()
   }
@@ -42,13 +51,13 @@ export default class ClienteCtrl implements IFab<Cliente>{
   public async getBody({ method, on, args }: param_body): body<Cliente> {
     if (method!='find_by_')
       throw new Error("Este método retorna uma lista.");
-    return Cliente.scope(
+    return await Cliente.scope(
       {method: [`${method}${on}`, args]}
       ).findOne()
   }
 
   public async getAllBodies(): Promise<Cliente[]> {
-    return Cliente.findAll({
+    return await Cliente.findAll({
       order: [
         sequelize.fn('concat', sequelize.col('nome'))
       ]
