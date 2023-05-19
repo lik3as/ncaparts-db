51cf5cb325543daac0d6f3bc809f94a4226ff5d6
Davi Almeida <lik3games@gmail.com>
2023-05-19 16:13:06 -0300

    1.4.6

--- lib/models/mercadoria.ts
+++ lib/models/mercadoria.ts
@@ -5,7 +5,6 @@ import {
 } from './index'
 
 import {
-  HasOne,
   PrimaryKey,
   ForeignKey,
   Column,
@@ -14,7 +13,6 @@ import {
   AutoIncrement,
   DataType,
   HasMany,
-  Scopes
 } from 'sequelize-typescript'
 
 
