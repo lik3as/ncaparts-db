e6d70f3fa87c731f4626c5ad0608c43e38fe608e
Davi Almeida <lik3games@gmail.com>
2023-06-07 17:17:16 -0300

    filter uniques

--- lib/models/mercadoria.ts
+++ lib/models/mercadoria.ts
@@ -14,6 +14,7 @@ import {
   DataType,
   HasMany,
   BelongsTo,
+  Unique,
 } from 'sequelize-typescript'
 
 
@@ -23,6 +24,10 @@ export class Mercadoria extends Model{
   @AutoIncrement
   @Column
   declare id: number;
+
+  @Unique(true)
+  @Column
+  declare nome: string;
 
   @Column
   @ForeignKey(() => Produto)
--- lib/services/srv.merc.ts
+++ lib/services/srv.merc.ts
@@ -5,6 +5,26 @@ import IFab, {param_body, param_bodies, body} from '../contracts/IServices'
 export default class MercadoriaCtrl implements IFab<Mercadoria>{
   constructor(){ }
 
+  async filterUniques(body: Object[] | Object): Promise<Object | Object[] | null> {
+    if (Array.isArray(body)) {
+      const filtered_map = await Promise.all(
+        body.map(async (merc) => {
+          return (await this.getBody({method: 'find_by_', on: 'unique', args: merc.nome})) == null;
+        })
+      )
+
+      return body.filter((_, i) => filtered_map[i]);
+    } else {
+      const merc = await this.getBody({method: 'find_by_', on: 'unique', args: (body as any).nome});
+      return merc == null
+      ?
+      body
+      :
+      null;
+    }
+
+  }
+
   async getAttr(name: string): Promise<Mercadoria[]> {
     return await Mercadoria.findAll({attributes: [name]});
   }
