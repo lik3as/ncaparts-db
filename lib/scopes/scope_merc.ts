bd17c481b3f5b4f30ce9bb9a38eaed1a8985a8ca
Davi Almeida <lik3games@gmail.com>
2023-06-26 12:01:26 -0300

    formated and removed a scope

--- lib/scopes/scope_merc.ts
+++ lib/scopes/scope_merc.ts
@@ -5,6 +5,7 @@ import { FindOptions, IncludeOptions, Op } from "sequelize";
 export const merc_scopes: ScopesOptionsGetter = () => ({
   find_by_unique(id_prod: number): FindOptions {
     return {
+      attributes: ['id', 'valor_real', 'valor_real_revenda', 'skus_relacionados', 'importada', 'disponivel', 'nome'],
       where: {
         id_produto: {
           [Op.eq]: id_prod
@@ -52,6 +53,7 @@ export const merc_scopes: ScopesOptionsGetter = () => ({
   find_by_related(sku: string): FindOptions & IncludeOptions {
     const sequelize = db
     return {
+      attributes: ['id', 'valor_real', 'valor_real_revenda', 'skus_relacionados', 'importada', 'disponivel', 'nome'],
       where: sequelize.literal(`'${sku}' = ANY(skus_relacionados)`),
       include: [{
         model: Produto,
@@ -88,6 +90,7 @@ export const merc_scopes: ScopesOptionsGetter = () => ({
   },
   find_by_name(name: string): FindOptions & IncludeOptions {
     return {
+      attributes: ['id', 'valor_real', 'valor_real_revenda', 'skus_relacionados', 'importada', 'disponivel', 'nome'],
       where: {
         nome: {
           [Op.iLike]: `%${name}%`
@@ -126,44 +129,4 @@ export const merc_scopes: ScopesOptionsGetter = () => ({
       }],
     }
   },
-  find_by_relatedArray(sku: string[]): FindOptions & IncludeOptions {
-    return {
-      where: {
-        skus_relacionados: {
-          [Op.any]: sku
-        }
-      },
-      include: [{
-        model: Produto,
-        as: 'produto',
-        attributes: ['id', 'sku', 'final', 'desc', 'imagens'],
-        include: [{
-          model: Produto,
-        }, {
-          model: Tipo,
-          as: 'tipo',
-          attributes: ['id', 'nome']
-        }, {
-          model: Subtipo,
-          as: 'subtipo',
-          attributes: ['id', 'nome']
-        }, {
-          model: Marca,
-          as: 'marca',
-          attributes: ['id', 'nome']
-        }, {
-          model: Modelo,
-          as: 'modelo',
-          attributes: ['id', 'nome']
-        }, {
-          model: Versao,
-          as: 'versao',
-          attributes: ['id', 'nome']
-        }],
-      }, {
-        model: Kit,
-        as: 'kit'
-      }],
-    }
-  }
-});
+})
